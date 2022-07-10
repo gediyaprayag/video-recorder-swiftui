3896b842e24b751656fe83543a842a07c8007aae
Prayag Gediya <prayaggediya60@gmail.com>
2022-07-10 10:35:43 +0530

    Initial Commit

---
+++ video-recorder-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  video-recorder-swiftui
+//
+//  Created by Prayag Gediya on 10/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ video-recorder-swiftui/video_recorder_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  video_recorder_swiftuiApp.swift
+//  video-recorder-swiftui
+//
+//  Created by Prayag Gediya on 10/07/22.
+//
+
+import SwiftUI
+
+@main
+struct video_recorder_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
